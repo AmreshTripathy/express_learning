64a5dd8c8e8cfb56948d89e9df02727bbcba935f
AmreshTripathy <amreshtripathy184@gmail.com>
2025-02-24 13:18:10 +0530

    scripts.js added

---
+++ scripts/sum.js
@@ -0,0 +1,11 @@
+console.log('Hi Welcome to noded js');
+
+setTimeout(() => {
+    console.log('message delayed for 2 seconds');
+}, 2000);
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(2, 3));
